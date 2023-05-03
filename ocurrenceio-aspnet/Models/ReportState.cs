13157272499401231ab7c32a0f1f21ea95d4c121
anagracio <anitagracio@hotmail.com>
2023-05-03 15:31:22 +0100

    Created the models

---
+++ ocurrenceio-aspnet/Models/ReportState.cs
@@ -0,0 +1,25 @@
+﻿namespace ocurrenceio_aspnet.Models {
+    /// <summary>
+    /// Represents the Report State Structure
+    /// </summary>
+    public class ReportState {
+        public ReportState() {
+            ListReport = new HashSet<Report>();
+        }
+        /// <summary>
+        /// Primary key for the Report State table
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// The Report state
+        /// </summary>
+        public string State { get; set; }
+
+        //***********************************
+
+        /// <summary>
+        /// List of reports associated to a state
+        /// </summary>
+        public ICollection<Report> ListReport { get; set; }
+    }
+}
