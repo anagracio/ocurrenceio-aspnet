39e23a43dc0f94950261f887fc2b0fc8c613aec0
Daniel Graça <danielgrascas@gmail.com>
2023-05-03 15:42:30 +0100

    Creation of database tables + migration
    Created database tables from Models and added the first migration

--- ocurrenceio-aspnet/Data/ApplicationDbContext.cs
+++ ocurrenceio-aspnet/Data/ApplicationDbContext.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ocurrenceio_aspnet.Models;
 
 namespace ocurrenceio_aspnet.Data {
     public class ApplicationDbContext : IdentityDbContext {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) {
         }
+
+        // Definition of project's table
+        public DbSet<Report> Report { get; set; }
+        public DbSet<ReportImage> ReportImage { get; set; } 
+        public DbSet<ReportState> ReportState { get; set; }
     }
 }
---
+++ ocurrenceio-aspnet/Data/Migrations/20230503143720_modelsCreation.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ocurrenceio_aspnet.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class modelsCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Latitude = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Longitude = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReportState",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportState", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReportImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReportFK = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReportImage_Report_ReportFK",
+                        column: x => x.ReportFK,
+                        principalTable: "Report",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReportReportState",
+                columns: table => new
+                {
+                    ListReportId = table.Column<int>(type: "int", nullable: false),
+                    ListReportStateId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportReportState", x => new { x.ListReportId, x.ListReportStateId });
+                    table.ForeignKey(
+                        name: "FK_ReportReportState_ReportState_ListReportStateId",
+                        column: x => x.ListReportStateId,
+                        principalTable: "ReportState",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReportReportState_Report_ListReportId",
+                        column: x => x.ListReportId,
+                        principalTable: "Report",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReportImage_ReportFK",
+                table: "ReportImage",
+                column: "ReportFK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReportReportState_ListReportStateId",
+                table: "ReportReportState",
+                column: "ListReportStateId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReportImage");
+
+            migrationBuilder.DropTable(
+                name: "ReportReportState");
+
+            migrationBuilder.DropTable(
+                name: "ReportState");
+
+            migrationBuilder.DropTable(
+                name: "Report");
+        }
+    }
+}
