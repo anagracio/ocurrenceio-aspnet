99faabf6120a33a423d382aa094067c3e1203ebf
Ana Grácio <anitagracio@hotmail.com>
2023-05-11 10:42:19 +0100

    Creation of basic entity controllers

---
+++ ocurrenceio-aspnet/Controllers/ReportStatesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ocurrenceio_aspnet.Data;
+using ocurrenceio_aspnet.Models;
+
+namespace ocurrenceio_aspnet.Controllers
+{
+    public class ReportStatesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReportStatesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ReportStates
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.ReportState.ToListAsync());
+        }
+
+        // GET: ReportStates/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.ReportState == null)
+            {
+                return NotFound();
+            }
+
+            var reportState = await _context.ReportState
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reportState == null)
+            {
+                return NotFound();
+            }
+
+            return View(reportState);
+        }
+
+        // GET: ReportStates/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReportStates/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,State")] ReportState reportState)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reportState);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reportState);
+        }
+
+        // GET: ReportStates/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.ReportState == null)
+            {
+                return NotFound();
+            }
+
+            var reportState = await _context.ReportState.FindAsync(id);
+            if (reportState == null)
+            {
+                return NotFound();
+            }
+            return View(reportState);
+        }
+
+        // POST: ReportStates/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,State")] ReportState reportState)
+        {
+            if (id != reportState.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reportState);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReportStateExists(reportState.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reportState);
+        }
+
+        // GET: ReportStates/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.ReportState == null)
+            {
+                return NotFound();
+            }
+
+            var reportState = await _context.ReportState
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reportState == null)
+            {
+                return NotFound();
+            }
+
+            return View(reportState);
+        }
+
+        // POST: ReportStates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.ReportState == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.ReportState'  is null.");
+            }
+            var reportState = await _context.ReportState.FindAsync(id);
+            if (reportState != null)
+            {
+                _context.ReportState.Remove(reportState);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReportStateExists(int id)
+        {
+          return _context.ReportState.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ocurrenceio-aspnet/Controllers/ReportsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ocurrenceio_aspnet.Data;
+using ocurrenceio_aspnet.Models;
+
+namespace ocurrenceio_aspnet.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReportsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reports
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Report.ToListAsync());
+        }
+
+        // GET: Reports/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Report == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Report
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return View(report);
+        }
+
+        // GET: Reports/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reports/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,Latitude,Longitude")] Report report)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(report);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(report);
+        }
+
+        // GET: Reports/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Report == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Report.FindAsync(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+            return View(report);
+        }
+
+        // POST: Reports/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Latitude,Longitude")] Report report)
+        {
+            if (id != report.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(report);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReportExists(report.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(report);
+        }
+
+        // GET: Reports/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Report == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Report
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return View(report);
+        }
+
+        // POST: Reports/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Report == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Report'  is null.");
+            }
+            var report = await _context.Report.FindAsync(id);
+            if (report != null)
+            {
+                _context.Report.Remove(report);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReportExists(int id)
+        {
+          return _context.Report.Any(e => e.Id == id);
+        }
+    }
+}
