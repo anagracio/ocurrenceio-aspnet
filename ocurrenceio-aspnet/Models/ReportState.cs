354a2335409857ad900a1275b9bb3e4321442097
Daniel Graça <danielgrascas@gmail.com>
2023-05-16 12:17:20 +0100

    Regexp and validations
    Model fields validation (required fields and error messages) and Regexp to prevent unwanted formats.

--- ocurrenceio-aspnet/Models/ReportState.cs
+++ ocurrenceio-aspnet/Models/ReportState.cs
@@ -1,18 +1,28 @@
-﻿namespace ocurrenceio_aspnet.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ocurrenceio_aspnet.Models {
     /// <summary>
     /// Represents the Report State Structure
     /// </summary>
     public class ReportState {
+        /// <summary>
+        /// Creates empty list for the reports
+        /// </summary>
         public ReportState() {
             ListReport = new HashSet<Report>();
         }
+
         /// <summary>
         /// Primary key for the Report State table
         /// </summary>
+        
         public int Id { get; set; }
         /// <summary>
         /// The Report state
         /// </summary>
+        
+        [Display(Name = "Estado")]
+        [Required(ErrorMessage = "O campo {0} é de preenchimento obrigatório.")]
         public string State { get; set; }
 
         //***********************************
