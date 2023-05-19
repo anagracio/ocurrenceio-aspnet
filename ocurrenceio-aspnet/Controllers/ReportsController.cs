aa6ba8ee567903483536747dd8ca9b6f19073465
Ana Grácio <anitagracio@hotmail.com>
2023-05-19 15:21:26 +0100

    Added the image in the Report View

--- ocurrenceio-aspnet/Controllers/ReportsController.cs
+++ ocurrenceio-aspnet/Controllers/ReportsController.cs
@@ -54,7 +54,7 @@ namespace ocurrenceio_aspnet.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Description,Latitude,Longitude")] Report report)
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,Latitude,Longitude")] Report report, List<IFormFile> images)
         {
             if (ModelState.IsValid)
             {
